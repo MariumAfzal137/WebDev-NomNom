abe8d4e672dfd470b6dc9ac2885926e9551368d1
SafaAbdulHai <94900423+SafaAbdulHai@users.noreply.github.com>
2022-10-08 11:26:54 +0500

    Revert "Add user crud"

--- WebProject/WebProject/backend/routes/user-routes.js
+++ WebProject/WebProject/backend/routes/user-routes.js
@@ -1,14 +1,11 @@
 import express from "express";
-import { getAllUser, signup , login, getProfile, getUserById, updateUser, deleteUser} from "../controllers/user-controller";
+import { getAllUser, signup , login, getProfile} from "../controllers/user-controller";
 import {verifyAccessToken} from "../middleware/check-auth";
 const router = express.Router();
 
-router.get("/getAllUsers", getAllUser);
+router.get("/", getAllUser);
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/getProfile", verifyAccessToken, getProfile);
-router.get("/getUser/:id", getUserById);
-router.post("/updateUser/:id", updateUser);
-router.get("/deleteUser/:id", deleteUser);
 
 export default router;
