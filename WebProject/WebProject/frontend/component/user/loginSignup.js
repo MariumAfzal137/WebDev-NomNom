b7d48ceb8919d08e11c583a6fbf9eda0452ab6ab
SafaAbdulHai <s.hai_18625@khi.iba.edu.pk>
2022-11-05 19:46:11 +0500

    frontend

---
+++ WebProject/WebProject/frontend/component/user/loginSignup.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from "react";
+import "./LoginSignup.css";
+
+
+const LoginSignup = () => {
+    return (
+       <Fragment>
+        <div className="LoginSignupContainer">
+            <div className="LoginSignupBox">
+                 <div>
+                    <div className="login_signup_toggle">
+                        
+                    </div>
+                 </div>
+            </div>
+        </div>
+        
+        </Fragment>
+    )
+}
+
+export default LoginSignup
