9745000171dd0cf1d71e2faf0ac9e36284b4483f
aimanmparacha <99040025+aimanmparacha@users.noreply.github.com>
2022-11-13 11:27:34 +0500

    Header and My profile page made

--- WebProject/WebProject/frontend/src/App.js
+++ WebProject/WebProject/frontend/src/App.js
@@ -1,15 +1,21 @@
-//import './App.css';
-import Login from './user/Login';
+import './App.css';
+import Header from './components/Header';
+import Profile from './components/Profile';
+//import Login from './user/Login';
+//import { BrowserRouter as Router }
 
 function App() {
-  return (
-    // <Router>
-    //     <Route exact path="/login" component={Login}/>
-    // </Router>
-    <div className="page">
-        <Login />
-    </div>
-  );
+
+  return <Profile/>;
+
+  // return (
+  //   // <Router>
+  //   //     <Route exact path="/login" component={Login}/>
+  //   // </Router>
+  //   <div className="page">
+  //       <Login />
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ WebProject/WebProject/frontend/src/components/Profile.js
@@ -0,0 +1,41 @@
+import { Container } from '@mui/system';
+import React from 'react'
+import Header from './Header';
+import "./Profile.css";
+
+//import {AppBar, Toolbar, Typography, Box,  InputBase, Button} from '@mui/material'
+
+export default function  Profile() {
+  return (
+
+        <>
+        <Header/>
+        <img id="profile" src="nomnomgrey.png" alt="Avatar"></img>
+
+        <h1 id="profile"> 
+        <ul>Safa Hai</ul>
+        <ul>@safahai</ul>
+        </h1>
+
+        <form id ="profile">  
+        <h6> Name</h6>  
+        <input id="profile" type="text" placeholder="Name"/>  <br/><br/>
+        <h6> Username</h6>  
+        <input id="profile" type="text" placeholder="Username"/>  <br/><br/>
+        <h6> Email</h6>  
+        <input id="profile" type="text" placeholder="Email"/>  <br/><br/>
+        <h6> Password</h6>  
+        <input id="profile" type="password" placeholder="Password"/> <br/><br/> <br/>
+    
+        
+        <input className="profile-btn" type="button" value="Save" onClick="create()"/>  
+        </form>  
+        
+
+        </>
+  )
+}
+
+//export default Profile
+
+
