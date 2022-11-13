898af2232ff15bbb49907f872b1871e87b6ea816
SafaAbdulHai <s.hai_18625@khi.iba.edu.pk>
2022-11-13 18:51:03 +0500

    Revert "Merge branch 'frontend' of https://github.com/MariumAfzal137/WebDev-NomNom into frontend"
    This reverts commit aac0948d5801e2c0a560a09565ce36ed7efbe40d, reversing
    changes made to 207c5ba47817322671f87c8258e9b576529e6174.

--- WebProject/WebProject/backend/routes/user-routes.js
+++ WebProject/WebProject/backend/routes/user-routes.js
@@ -1,14 +1,12 @@
 import express from "express";
-import { getAllUser, signup , login, getProfile, getUserById, updateUser, deleteUser} from "../controllers/user-controller";
-import {verifyAccessToken} from "../middleware/check-auth";
+import { getAllUser, signup , login, getProfile} from "../controllers/user-controller";
+import {verifyAccessToken,verifyAdminAccessToken} from "../middleware/check-auth";
 const router = express.Router();
 
-router.get("/getAllUsers", getAllUser);
+
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/getProfile", verifyAccessToken, getProfile);
-router.get("/getUser/:id", getUserById);
-router.post("/updateUser/:id", updateUser);
-router.get("/deleteUser/:id", deleteUser);
+router.get("/getProfile",verifyAccessToken || verifyAdminAccessToken, getProfile);
+router.get("/getAllUsers",verifyAdminAccessToken,getAllUser);
 
 export default router;
