cda943e93086d1f87ee5ff70b98b72ca07df0efd
SafaAbdulHai <s.hai_18625@khi.iba.edu.pk>
2022-12-23 17:30:33 +0500

    try to fix my recipe

--- WebProject/WebProject/frontend/src/components/userRecipe/myrecipeCard.js
+++ WebProject/WebProject/frontend/src/components/userRecipe/myrecipeCard.js
@@ -3,7 +3,8 @@ import {Link} from 'react-router-dom';
 import RecipeDetail from "../RecipeDetail";
 
 const Card = ({rItems}) => { 
-
+   
+ 
     return (
     <>
       {rItems.map((rItems, index) => {
